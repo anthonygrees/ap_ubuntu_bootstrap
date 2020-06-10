931345605091f53fb1d813e60e06c083d82cb248
anthonygrees <anthony_rees01@hotmail.com>
2020-06-10 16:06:28 +1000

    initial

---
+++ metadata.rb
@@ -0,0 +1,20 @@
+name 'ap_ubuntu_bootstrap'
+maintainer 'The Authors'
+maintainer_email 'anthony@chef.io'
+license 'All Rights Reserved'
+description 'Installs/Configures and bootstraps an Ubuntu Node'
+long_description 'Installs/Configures and bootstraps an Ubuntu Node'
+version '0.1.0'
+chef_version '>= 12.14' if respond_to?(:chef_version)
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/bjc_linux_bootstrap/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/bjc_linux_bootstrap'
